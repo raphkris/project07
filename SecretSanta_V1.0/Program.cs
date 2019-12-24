cf29f75bf74cec4a7f01b1bf2ebd10def9094b0e
Raphael Mangubat <raphaelmangubat@gmail.com>
2019-12-24 17:22:13 -0600

    saving changes.

---
+++ SecretSanta_V1.0/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SecretSanta_V1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Introduce program only pulls secret santa names
+
+            // Create group - ask for group name (no spaces/must match regex/pattern/etc)
+
+            // Create empty array with same name as group
+
+            // Display group name
+
+            // Prompt for member name
+
+            // while loop checks amount of members/rows added to group/array
+
+            // Prompt for member name
+
+            // if members/rows >= 5, prompt to add another name OR finish group and draw
+
+        }
+    }
+}
